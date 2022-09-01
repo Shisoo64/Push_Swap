9085573d222e953156eed8d072a17d88894ffb8f
Romain Laforge <rlaforge@1G2.42angouleme.fr>
2022-09-01 17:16:27 +0200

    Sorting sort of working

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:39:33 by rlaforge          #+#    #+#             */
-/*   Updated: 2022/08/30 16:26:22 by rlaforge         ###   ########.fr       */
+/*   Updated: 2022/09/01 17:14:54 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,20 @@ void print_stack(t_stacks *stacks)
 
     tmp = stacks->a_head;
     ft_printf("\n***Printing the stacks***\n");
-    ft_printf("Stack A :  ");
-	while (tmp->next != NULL)
+    ft_printf("Stack A : * ");
+	while (tmp != NULL)
     {
         ft_printf("%d * ", tmp->data);
 		tmp = tmp->next;
     }
-    ft_printf("%d\n", tmp->data);
     tmp = stacks->b_head;
-    ft_printf("Stack B :  ");
-	while (tmp->next != NULL)
+    ft_printf("\nStack B : * ");
+	while (tmp != NULL)
     {
         ft_printf("%d * ", tmp->data);
 		tmp = tmp->next;
     }
-    ft_printf("%d\n", tmp->data);
-    ft_printf("*************************\n\n");
+    ft_printf("\n*************************\n\n");
 }
 
 t_node	*newnode(int data)
@@ -49,15 +47,16 @@ t_node	*newnode(int data)
 	return (node);
 }
 
-void	pushfront(t_node **new, t_node **head, char id)
+void	pushfront(t_node **src, t_node **dst, char id)
 {
     t_node	*tmp;
 
+
     ft_printf("p%c\n", id);
-    tmp = (*new)->next;
-	(*new)->next = *head;
-	*head = *new;
-    *new = tmp;
+    tmp = (*src)->next;
+	(*src)->next = *dst;
+	*dst = *src;
+    *src = tmp;
 }
 
 void	rotate(t_node **head, char id)
@@ -65,10 +64,11 @@ void	rotate(t_node **head, char id)
     t_node	*tail;
     t_node	*tmp;
 
+	if (!*head || !(*head)->next)
+		return ;
     ft_printf("r%c\n", id);
     tail = *head;
     tmp = *head;
-    
     *head = (*head)->next;
     while (tail->next != NULL)
 		tail = tail->next;
@@ -80,47 +80,53 @@ void	addback(t_node **head, t_node *new)
 {
 	t_node	*tmp;
 
-	if (!head || !*head)
+	if (!*head)
 	{
 		*head = new;
 		return ;
 	}
 	tmp = *head;
-	while (tmp->next != NULL)
+	while (tmp && tmp->next != NULL)
 		tmp = tmp->next;
 	tmp->next = new;
 }
 
 void	swapstacks(t_node **head_a, t_node **head_b)
 {
-    while ((*head_a)->next != NULL)
+    while (*head_a != NULL)
         pushfront(head_a, head_b, 'b');
 }
+
 void    sort(t_stacks *stacks)
 {
 	t_node	*tmpa;
 	t_node	*tmpb;
     int x;
+    int i;
+
 
     ft_printf("\n***SORTING THE STACKS***\n\n");
-    tmpa = stacks->a_head;
     x = -1;
     while (++x < 32)
     {
-        while (tmpa->next != NULL)
+        i = -1;
+        tmpa = stacks->a_head;
+        while (++i < stacks->len)
         {
+            if (!tmpa)
+                break;
             if ((tmpa->data >> x) & 1)
-                pushfront(&stacks->a_head, &stacks->b_head, 'b');
-            else
                 rotate(&stacks->a_head, 'a');
+            else   
+                pushfront(&stacks->a_head, &stacks->b_head, 'b');
             tmpa = stacks->a_head;
             print_stack(stacks);
         }
         tmpb = stacks->b_head;
-        while (tmpb->next != NULL)
+        while (tmpb != NULL)
         {
-		    //tmpb = tmpb->next;
             pushfront(&stacks->b_head, &stacks->a_head, 'a');
+            tmpb = stacks->b_head;
         }
         print_stack(stacks);
     }
@@ -135,16 +141,16 @@ int main(int ac, char **av)
 	i = 1;
     stacks.len = ac;
     stacks.a_head = newnode(ft_atoi(av[1]));
-    stacks.b_head = newnode(0);
+    stacks.b_head = NULL;
 	while (++i != stacks.len)
         addback(&stacks.a_head, newnode(ft_atoi(av[i])));
     print_stack(&stacks);
-    swapstacks(&stacks.a_head, &stacks.b_head);
+    /*swapstacks(&stacks.a_head, &stacks.b_head);
     print_stack(&stacks);
     swapstacks(&stacks.b_head, &stacks.a_head);
     print_stack(&stacks);
     rotate(&stacks.a_head, 'a');
-    print_stack(&stacks);
+    print_stack(&stacks);*/
     sort(&stacks);
     print_stack(&stacks);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: rlaforge <rlaforge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 15:39:33 by rlaforge          #+#    #+#             */
-/*   Updated: 2022/08/30 15:04:05 by rlaforge         ###   ########.fr       */
+/*   Updated: 2022/09/01 16:23:11 by rlaforge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,54 @@ x = -1;
     }
     
 
+*/
+/*
+int    get_bitnbr(int size)
+{
+    int bitnbr;
+    int bit_pow;
+
+    bit_pow = 1;
+    bitnbr = 0;
+    while ((size / bit_pow) != 0)
+    {
+        bit_pow *= 2;
+        bitnbr++;
+    }
+
+    return (bitnbr);
+}
+
+void    sort(t_stacks *stacks)
+{
+	t_node	*tmpa;
+	t_node	*tmpb;
+    int x;
+    int bitnbr;
+
+    ft_printf("\n***SORTING THE STACKS***\n\n");
+    bitnbr = get_bitnbr(stacks->len);
+    tmpa = stacks->a_head;
+    x = -1;
+    while (++x < bitnbr)
+    {
+        while (tmpa->next != NULL)
+        {
+            if ((tmpa->id >> x) & 1)
+                pushfront(&stacks->a_head, &stacks->b_head, 'b');
+            else
+                rotate(&stacks->a_head, 'a');
+            tmpa = stacks->a_head;
+            print_stack(stacks);
+        }
+        tmpb = stacks->b_head;
+        while (tmpb->next != NULL)
+        {
+            pushfront(&stacks->b_head, &stacks->a_head, 'a');
+            tmpb = tmpb->next;
+        }
+        print_stack(stacks);
+    }
+    ft_printf("\n************************\n");
+}
 */
